1e2a678f90459d6c9541d2eb66c767e67a226c1f
Mateusz <laverok666@gmail.com>
2019-05-24 19:59:47 +0200

    Changed play method so its much simpler

--- GuitarEffects.py
+++ GuitarEffects.py
@@ -2,10 +2,9 @@ import wave
 import pyaudio
 import struct
 import math
-from numpy.fft import *
-import matplotlib.pyplot as plt
+import numpy as np
 import WavUtils as util
 
-wavTrack = util.WavFile("./sample/stereo/Roundabout.wav")
+wavTrack = util.WavFile("./sample/stereo/SanSalvadorr.wav")
 
 wavTrack.play()
--- WavUtils.py
+++ WavUtils.py
@@ -2,6 +2,8 @@ import wave
 import pyaudio
 import struct
 import math
+import numpy as np
+import simpleaudio as sa
 
 class WavFile:
 
@@ -11,7 +13,7 @@ class WavFile:
         """Initialize audio stream and save all information"""
         self.wav = wave.open(file, 'r')
         self.fileName = file
-        
+
         # framerate
         self.fs = self.wav.getframerate()
 
@@ -29,35 +31,19 @@ class WavFile:
 
         # save data as an array
         self.samples = []
+
+      
         for i in range(0, self.frames):
             wavData = self.wav.readframes(1)
-            data = struct.unpack("%ih"%2, wavData)
+            data = struct.unpack("%ih"%self.channels, wavData)
             self.samples.append(int(data[0]))
 
+        self.samples = np.array(self.samples)
         self.wav.close()
 
 
     def play(self):
         """Play the file"""
-        wav = wave.open(self.fileName, 'r')
-        p = pyaudio.PyAudio()
+        play = sa.play_buffer(self.samples, self.channels, self.bytes, self.fs)
+        play.wait_done()
 
-        # opens the audio stream
-        stream = p.open(
-            format = p.get_format_from_width(self.bytes),
-            channels = self.channels,
-            rate = self.fs,
-            output = True
-        )
-
-        # reads the audio stream until the end of it
-        wavData = wav.readframes(self.chunk)
-        while len(wavData) > 0:
-            stream.write(wavData)
-            wavData = wav.readframes(self.chunk)
-        
-        # clean-up
-        wav.close()
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
