2b0db5399e44afa55719e146dd3bdcc1e308fe80
Mateusz <laverok666@gmail.com>
2019-05-26 18:59:46 +0200

    Added basic menu for opening files

--- Gui.py
+++ Gui.py
@@ -1,4 +1,7 @@
 import wx
+import tkinter as tk
+from tkinter import filedialog
+import WavUtils as util
 
 class MainWindow(wx.Frame):
 
@@ -16,16 +19,29 @@ class MainWindow(wx.Frame):
 
         # File/Open
         fileOpen = fileMenu.Append(wx.ID_NEW, "Open")
+        self.Bind(wx.EVT_MENU, self.openfile, fileOpen)
 
         # File/Exit
         fileExit = fileMenu.Append(wx.ID_EXIT, "Exit")
         self.Bind(wx.EVT_MENU, self.closewindow, fileExit)
 
         menuBar.Append(fileMenu, 'File')
-        
+
         self.SetMenuBar(menuBar)
 
 
     def closewindow(self, event):
         """Close a window"""
         self.Destroy()
+
+    def openfile(self, event):
+
+        root = tk.Tk()
+        root.withdraw()
+
+        dialogTitle = "Select a .wav file"
+        ftypes = [("WAV", ".wav")]
+
+        filepath = filedialog.askopenfilename(initialdir = "./", title = dialogTitle, filetypes = ftypes)
+
+        root.destroy()
