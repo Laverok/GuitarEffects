f54f4ec7c915f39f2610f5479e8d02541d012ddf
Mateusz <laverok666@gmail.com>
2019-05-27 11:12:28 +0200

    Added static boxes

--- Gui.py
+++ Gui.py
@@ -31,9 +31,28 @@ class MainWindow(wx.Frame):
 
         menuBar.Append(fileMenu, 'File')
 
-        self.SetMenuBar(menuBar)
+        # Status boxes
+        boxWidth = 110
+        boxHeight = 300
+        boxSize = (boxWidth, boxHeight)
 
+        initialWidth = 1
+        initialHeight = 0
+        pos0 = (initialWidth, initialHeight)
+        pos1 = (initialWidth + boxWidth, initialHeight)
+        pos2 = (initialWidth + 2*boxWidth, initialHeight)
+        pos3 = (initialWidth + 3*boxWidth, initialHeight)
+        pos4 = (initialWidth + 4*boxWidth, initialHeight)
 
+        echoPlay = wx.StaticBox(mainPanel, wx.ID_NEW, "Play", pos = pos0, size = boxSize )
+        echoBox = wx.StaticBox(mainPanel, wx.ID_NEW, "Echo", pos = pos1, size = boxSize )
+        echoDist = wx.StaticBox(mainPanel, wx.ID_NEW, "Distortion", pos = pos2, size = boxSize )
+        echoTemp1 = wx.StaticBox(mainPanel, wx.ID_NEW, "Effect no. 3", pos = pos3, size = boxSize )
+        echoTemp2 = wx.StaticBox(mainPanel, wx.ID_NEW, "Effect no. 4", pos = pos4, size = boxSize )
+
+  
+
+    
     def closewindow(self, event):
         """Close a window"""
         self.Destroy()
